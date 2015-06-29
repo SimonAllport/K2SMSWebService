d80017404e6c1b8492ed779a2c2c8bed781d3322
SimonAllport <simon@greeneggs.co.uk>
2015-06-29 15:56:57 +0100

    CODE

---
+++ SendSMS/SendMessage.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="SendMessage.asmx.cs" Class="SendSMS.Service1" %>
---
+++ SendSMS/SendMessage.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using K2SMS.SendSMS;
+
+namespace SendSMS
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void SendMessage(string Mobile,string Message)
+        {
+            K2SMS.SendTxt.SendMessage(Message, Mobile);
+
+        }
+    }
+}
